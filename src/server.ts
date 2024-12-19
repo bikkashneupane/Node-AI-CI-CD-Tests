5b4853a6f10e3757604f75a44fb6ee6da195013b
bikashneupane17 <edubikkash@gmail.com>
2024-12-19 17:24:30 +1100

    CI yml change

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,19 @@
-console.log("test");
+import express, { Request, Response } from "express";
+
+const app = express();
+const port = 3000;
+
+// Middleware to parse JSON
+app.use(express.json());
+
+// Basic route
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello, world!");
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
+
+export default app;
