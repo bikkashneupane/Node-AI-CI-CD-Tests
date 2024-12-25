eb609404c0da031236579e25499427b821b9091b
bikashneupane17 <edubikkash@gmail.com>
2024-12-25 13:30:36 +1100

    Server tests

--- tests/server.test.ts
+++ tests/server.test.ts
@@ -1,7 +1,15 @@
+import request from "supertest";
+import app from "../src/server";
+
 describe("Server Test Cases", () => {
   it("Should throw an error", () => {
     expect(() => {
       throw new Error("Something failed"); // Fixed typo here
     }).toThrow("Something failed"); // Check that the error message is correct
+  });
+
+  it("Should get status 200 from server root endpoint", async () => {
+    const response = await request(app).get("/");
+    expect(response.status).toBe(200);
   });
 });
