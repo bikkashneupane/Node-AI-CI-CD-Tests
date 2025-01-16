26b63468710807153dc72958eba5ea21791f8f16
bikashneupane17 <edubikkash@gmail.com>
2025-01-16 21:45:04 +1100

    Cache added back

--- api-gateway/src/config/envConfig.ts
+++ api-gateway/src/config/envConfig.ts
@@ -1,5 +1,5 @@
 export const envConfig = {
-  MONGO_URI: process.env.MONGO_URI as string,
+  MONGO_URI: (process.env.MONGO_URI as string) || "",
   SALT: process.env.SALT,
   OPENAI_API_KEY: process.env.OPENAI_API_KEY || "",
 };
