9d591ac2fd61261caacb240a2b68d69adfbf08fd
bikashneupane17 <edubikkash@gmail.com>
2025-01-16 22:07:31 +1100

    ci cache removed + envconfig fallback

--- api-gateway/src/config/envConfig.ts
+++ api-gateway/src/config/envConfig.ts
@@ -1,5 +1,7 @@
 export const envConfig = {
-  MONGO_URI: (process.env.MONGO_URI as string) || "",
-  SALT: process.env.SALT,
+  MONGO_URI:
+    (process.env.MONGO_URI as string) ||
+    "mongodb://localhost:27017/ultimate_node",
+  SALT: process.env.SALT || 10,
   OPENAI_API_KEY: process.env.OPENAI_API_KEY || "",
 };
