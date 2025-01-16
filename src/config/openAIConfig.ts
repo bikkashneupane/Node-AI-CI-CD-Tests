51d001e99ddf1c451246e4bc3eca6f6df87e6aa0
bikashneupane17 <edubikkash@gmail.com>
2025-01-16 20:46:27 +1100

    repo name change test

--- src/config/openAIConfig.ts
+++ src/config/openAIConfig.ts
@@ -2,7 +2,7 @@ import { OpenAI } from "openai";
 import { envConfig } from "./envConfig";
 
 const openai = new OpenAI({
-  apiKey: envConfig.OPENAI_API_KEY as string,
+  apiKey: envConfig.OPENAI_API_KEY,
 });
 
 export default openai;
--- src/routes/chat.ts
+++ src/routes/chat.ts
@@ -13,13 +13,14 @@ router.post("/", async (req: Request, res: Response, next: NextFunction) => {
 
   try {
     const response = await openai.chat.completions.create({
-      model: "gpt-4o",
+      model: "gpt-3.5-turbo",
+      store: true,
       messages: [{ role: "user", content: prompt }],
     });
 
-    console.log(response?.choices[0]?.message?.content);
+    console.log(response?.choices[0]?.message);
 
-    res.json({ response: response?.choices[0]?.message?.content });
+    res.json({ response: response?.choices[0]?.message });
   } catch (error: any) {
     console.error("Error:", error.response?.data || error.message);
     next(error);
