07e473fc5e097c8fa08d4e5b65486222143ebd53
bikashneupane17 <edubikkash@gmail.com>
2025-01-17 17:53:41 +1100

    tsconfig change

--- book-service/tests/server.test.ts
+++ book-service/tests/server.test.ts
@@ -1,5 +1,5 @@
 import request from "supertest";
-import app from "../src/server";
+import app from "../src";
 import { connectMongo } from "../src/config/mongo";
 
 jest.mock("../src/config/mongo", () => ({
